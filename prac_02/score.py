f03b068644f8c3431e4d423587bbc3b68a46576a
daniel norman <Danielnorman247@gmail.com>
2025-06-01 18:46:11 +1000

    Refactor score file to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,17 +1,18 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+""" score """
+import random
+def main():
+    # result = convert_score_to_result(float(input("Enter score: ")))
+    result = convert_score_to_result(random.randint(0, 100))
+    print(f"Your result is: {result}")
 
-# TODO: Fix this!
+def convert_score_to_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
-
+main()
