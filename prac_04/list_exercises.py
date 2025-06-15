89b933ae345f97cfe546727c8e87ce6cfbc3ac83
daniel norman <Danielnorman247@gmail.com>
2025-06-15 17:51:52 +1000

    Add list_exorcises with complete Basic list operations

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,23 @@
+"""Basic list operations"""
+
+def main():
+    """ask user for 5 numbers then display the 1st, last, min, max, and average"""
+    numbers = populate_numbers() # get 5 numbers from user and return in list
+
+    #print miscellaneous information
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+
+
+def populate_numbers():
+    """gets user to input 5 numbers and adds them to list"""
+    numbers = []
+    for i in range(0, 5): # i is not used. Is there a better way to do this without creating an obsolete variable?
+        number = int(input("Enter number: ")) # get number from user
+        numbers.append(number) # add number to list
+    return numbers # return list
+
+main()
