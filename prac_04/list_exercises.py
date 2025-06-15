279b8bd7880d5ec3c8f75b4e299bf13bfc6c02f4
daniel norman <Danielnorman247@gmail.com>
2025-06-15 18:04:42 +1000

    Add woefully inadequate security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,15 +1,24 @@
 """Basic list operations"""
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
 def main():
     """ask user for 5 numbers then display the 1st, last, min, max, and average"""
-    numbers = populate_numbers() # get 5 numbers from user and return in list
 
-    #print miscellaneous information
-    print(f"The first number is {numbers[0]}")
-    print(f"The last number is {numbers[-1]}")
-    print(f"The smallest number is {min(numbers)}")
-    print(f"The largest number is {max(numbers)}")
-    print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+    # check if user is allowed
+    if is_authorised_user():
+
+        print("Access granted")
+        numbers = populate_numbers() # get 5 numbers from user and return in list
+
+        #print miscellaneous information
+        print(f"The first number is {numbers[0]}")
+        print(f"The last number is {numbers[-1]}")
+        print(f"The smallest number is {min(numbers)}")
+        print(f"The largest number is {max(numbers)}")
+        print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+    else:
+        print("Access denied")
 
 
 def populate_numbers():
@@ -19,5 +28,9 @@ def populate_numbers():
         number = int(input("Enter number: ")) # get number from user
         numbers.append(number) # add number to list
     return numbers # return list
+
+def is_authorised_user():
+    """is user inputted username in authorised user list"""
+    return input("username: ") in usernames
 
 main()
