afbad15c21fd164ae809643c27fedba7c5cb62f5
daniel norman <Danielnorman247@gmail.com>
2025-07-20 22:04:07 +1000

    Add saving and loading projects

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,24 +1,30 @@
 """
 Project Management exercise
-Guess time: 45m
+Guess time: 45m   ->  changed to 1.5 hours 23 minutes in
 Actual time:
 """
 
+import datetime
+
+from prac_03.capitalist_conrad import out_file
+
 DEFAULT_FILE = "projects.txt"
-CHOICE_MESSAGE = "Select a Menu"\
-                 "L - Load Projects\nS - Save Projects\n"\
-                 "D - Display Projects\nF - Filter Projects\n"\
-                 "A - Add Project\nU - Update Project\n"\
-                 "Q - Quit"
+CHOICE_MESSAGE = "Select a Menu" \
+                 "L - Load Projects\nS - Save Projects\n" \
+                 "D - Display Projects\nF - Filter Projects\n" \
+                 "A - Add Project\nU - Update Project\n" \
+                 "Q - Quit\n"
 
 
 def main():
+    projects = [] # will NOT be global but most funcs will need access to it
+    get_projects_from_file(projects, DEFAULT_FILE)
     choice = input(CHOICE_MESSAGE).upper()  # The desired selected menu
     while choice != "Q":
         if choice == "L":
-            load_projects()
+            load_projects(projects)
         elif choice == "S":
-            save_projects()
+            save_projects(projects)
         elif choice == "D":
             display_projects()
         elif choice == "F":
@@ -33,12 +39,38 @@ def main():
     # To save or not to save
 
 
-def load_projects():
-    pass
+def get_projects_from_file(projects, file_name):
+    """get projects from file and add to list"""
+    with open(file_name, "r") as file:
+        file.readline()  # skip header
+        for line in file:
+            parts = line.split("\t")  # split by the tabs
+            name = parts[0]
+            start_date = datetime.datetime.strptime(parts[1], "%d/%m/%Y").date()  # get date in date object format
+            priority = int(parts[2])
+            cost_estimate = float(parts[3])
+            completion_percentage = float(parts[4])
+            projects.append((name, start_date, priority, cost_estimate, completion_percentage))
+
+def give_projects_to_file(projects, file_name):
+    """get projects from list and add to file"""
+    with open(file_name, "w") as output_file:
+        print("Name	Start Date  Priority	Cost Estimate	Completion Percentage", file=output_file) # add header
+        for project in projects:
+            date = datetime.datetime.strftime(project[1], "%d/%m/%Y")
+            line = "\t".join([project[0],date , project[2], project[3], project[4]])
+            print(line,file=output_file)
+
+def load_projects(projects):
+    """get user file name and send to get_projects_from_file"""
+    file_name = input("What file would you like to load:\n")
+    get_projects_from_file(projects, file_name)
 
 
-def save_projects():
-    pass
+def save_projects(projects):
+    """get user file name and send to give_projects_to_file"""
+    file_name = input("What file would you like to load:\n")
+    give_projects_to_file(projects, file_name)
 
 
 def display_projects():
